6ef7a8ebb122dc443c3a837a8024d581a3acc4a0
AdmBrookhavenScripts <168764106+AdmBrookhavenScripts@users.noreply.github.com>
2026-06-19 14:49:51 -0300

    Add files via upload

--- assetd.py
+++ assetd.py
@@ -320,7 +320,8 @@ async def process_hls_playlist(session: aiohttp.ClientSession, m3u8_path: str, b
             parsed_master = urlparse(master_url)
             
             if not urlparse(target_path).query:
-                joined = urlunparse(parsed_joined._replace(query=parsed_master.query))
+                if parsed_joined.netloc == parsed_master.netloc:
+                    joined = urlunparse(parsed_joined._replace(query=parsed_master.query))
                 
             return joined
 
@@ -333,11 +334,10 @@ async def process_hls_playlist(session: aiohttp.ClientSession, m3u8_path: str, b
             internal_m3u8_content = m3u8_content
         else:
             if "{$RBX-BASE-URI}" in best_playlist_url and rbx_base_uri:
-                replaced_url = best_playlist_url.replace(
+                best_playlist_url = best_playlist_url.replace(
                     "{$RBX-BASE-URI}",
                     rbx_base_uri.rstrip("/")
                 )
-                best_playlist_url = get_url_with_auth(replaced_url, "", base_url)
             else:
                 best_playlist_url = get_url_with_auth(
                     base_url,
