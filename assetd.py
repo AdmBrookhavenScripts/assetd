198917fa17b4d97b050baf597e23dd12f53107cd
AdmBrookhavenScripts <168764106+AdmBrookhavenScripts@users.noreply.github.com>
2026-06-20 19:01:33 -0300

    Add files via upload

--- assetd.py
+++ assetd.py
@@ -411,7 +411,9 @@ async def process_hls_playlist(session: aiohttp.ClientSession, m3u8_path: str, b
 
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
-            "Cookie": f".ROBLOSECURITY={ROBLOX_COOKIE}" if ROBLOX_COOKIE else ""
+            "Cookie": f".ROBLOSECURITY={ROBLOX_COOKIE}" if ROBLOX_COOKIE else "",
+            "Origin": "https://www.roblox.com",
+            "Referer": "https://www.roblox.com/"
         }
 
         if not best_playlist_url:
