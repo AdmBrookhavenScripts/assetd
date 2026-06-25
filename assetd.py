647ab380c1971e3016b2ff46b12371a187b95731
AdmBrookhavenScripts <168764106+AdmBrookhavenScripts@users.noreply.github.com>
2026-06-25 13:49:05 -0300

    Add files via upload

--- assetd.py
+++ assetd.py
@@ -730,7 +730,7 @@ async def download_core(session: aiohttp.ClientSession, asset_id: str):
                 break
 
     if not asset_url:
-        msg = f"Asset ''{asset_id}'' - Asset inacessível: O asset provavelmente foi excluído permanentemente."
+        msg = f"Asset ''{asset_id}'' inacessível: O asset provavelmente foi excluído permanentemente."
         logger.error(msg)
         return None, msg
 
